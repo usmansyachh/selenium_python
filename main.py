a801ce4a24c7423a0bf94ff067e286ceead3b6f5
Usman <usmansyach123@gmail.com>
2025-03-25 10:58:57 +0700

    selenium dasar

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello, Selenium!")
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+'''
+- inisialisasi path chrome driver
+'''
+PATH = "C:\\Webdriver\chromedriver.exe" 
+service = Service(PATH)
+
+'''
+- insisialisasi url untuk web yang hendak di tes
+- driver.tittle untuk debug 
+'''
+driver = webdriver.Chrome(service=service)
+driver.get("https://techwithtim.net")
+print(driver.title)
+driver.quit()
